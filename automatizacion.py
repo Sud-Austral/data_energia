22e8e20b879a80f4401d595083484b6852433b27
gholley4 <gholley42@gmail.com>
2021-12-07 16:42:11 -0300

    Intento Actions 5

--- automatizacion.py
+++ automatizacion.py
@@ -6,7 +6,7 @@ def url():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
     url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones.json/'
     get_url = f"{url}?auth_key={api_auth}&limit=5000"
-return get_url
+    return get_url
 
 def proceso():
     response = requests.get(get_url)

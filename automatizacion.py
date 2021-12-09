e9e39dd26a3fdfdb4eaa83118dc200ef15a24668
gholley4 <gholley42@gmail.com>
2021-12-09 16:31:06 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -13,5 +13,17 @@ def proceso():
     df.columns = result["headers"]
     df.to_excel("combustibles_vehicular_estaciones.xlsx", index = False)
 
+def proceso2():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/diesel.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones.xlsx", index = False)
+
 if __name__ == '__main__':
     proceso();
+    proceso2();

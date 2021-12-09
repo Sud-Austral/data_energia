603d195bdebabcd84f2ad0555f8d252f61c52c1f
gholley4 <gholley42@gmail.com>
2021-12-09 16:38:20 -0300

    Descarga multiple 2

--- automatizacion.py
+++ automatizacion.py
@@ -24,6 +24,66 @@ def proceso2():
     df.columns = result["headers"]
     df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
 
+def proceso3():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gasolina93.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+
+def proceso4():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gasolina85.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+
+def proceso5():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gasolina97.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+
+def proceso6():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/glp.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+
+def proceso7():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gnc.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+
 if __name__ == '__main__':
     proceso();
     proceso2();
+    proceso3();
+    proceso4();
+    proceso5();
+    proceso6();
+    proceso7();

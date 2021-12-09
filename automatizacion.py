5a444a053e228f5318a899448c8b0255bce39f16
gholley4 <gholley42@gmail.com>
2021-12-09 16:45:15 -0300

    Multiple descargas 3

--- automatizacion.py
+++ automatizacion.py
@@ -33,18 +33,18 @@ def proceso3():
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]
-    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+    df.to_excel("combustibles_vehicular_estaciones_gasolina93.xlsx", index = False)
 
 def proceso4():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
-    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gasolina85.json/'
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones/gasolina95.json/'
     get_url = f"{url}?auth_key={api_auth}&limit=5000"
 
     response = requests.get(get_url)
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]
-    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+    df.to_excel("combustibles_vehicular_estaciones_gasolina95.xlsx", index = False)
 
 def proceso5():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
@@ -55,7 +55,7 @@ def proceso5():
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]
-    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+    df.to_excel("combustibles_vehicular_estaciones_gasolina97.xlsx", index = False)
 
 def proceso6():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
@@ -66,7 +66,7 @@ def proceso6():
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]
-    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+    df.to_excel("combustibles_vehicular_estaciones_glp.xlsx", index = False)
 
 def proceso7():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
@@ -77,7 +77,7 @@ def proceso7():
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]
-    df.to_excel("combustibles_vehicular_estaciones_diesel.xlsx", index = False)
+    df.to_excel("combustibles_vehicular_estaciones_gnc.xlsx", index = False)
 
 if __name__ == '__main__':
     proceso();

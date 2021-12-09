8aa4071b3ac538a90ea5d1f1b18ca11293439d12
gholley4 <gholley42@gmail.com>
2021-12-09 16:26:43 -0300

    Update automatizacion.py

--- automatizacion.py
+++ automatizacion.py
@@ -2,14 +2,12 @@ import requests
 import json
 import pandas as pd
 
-def url():
+def proceso():
     api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
     url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones.json/'
     get_url = f"{url}?auth_key={api_auth}&limit=5000"
-    return get_url
 
-def proceso():
-    response = requests.get(url())
+    response = requests.get_url
     result = response.json(strict=False)
     df = pd.DataFrame(result["data"])
     df.columns = result["headers"]

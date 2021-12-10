54ae572f627dba62ae7b2a6915213fd9e966fb56
gholley4 <gholley42@gmail.com>
2021-12-10 12:45:20 -0300

    Actualizacion action 1

--- automatizacion.py
+++ automatizacion.py
@@ -80,6 +80,72 @@ def proceso7():
     df.columns = result["headers"]
     df.to_excel("combustibles_vehicular_estaciones_gnc.xlsx", index = False)
 
+def proceso8():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/emisiones.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("combustibles_vehicular_estaciones_gnc.xlsx", index = False)
+
+def proceso9():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/capacidad-instalada/v1/convencional.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("capacidad-instalada_convencional.xlsx", index = False)
+
+def proceso10():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/capacidad-instalada/v1/enoperacion.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("capacidad-instalada_enoperacion.xlsx", index = False)
+
+def proceso11():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/capacidad-instalada/v1/enpruebas.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("capacidad-instalada_enpruebas.xlsx", index = False)
+
+def proceso12():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/capacidad-instalada/v1/ernc.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("capacidad-instalada_ernc.xlsx", index = False)
+
+def proceso13():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/capacidad-instalada/v1/sistemas.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    result = response.json(strict=False)
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+    df.to_excel("capacidad-instalada_sistemas.xlsx", index = False)
+
 if __name__ == '__main__':
     try:
         proceso();
@@ -139,6 +205,60 @@ if __name__ == '__main__':
     except:
         try:
             proceso7
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+    
+    try:
+        proceso8();
+    except:
+        try:
+            proceso8
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+    
+    try:
+        proceso9();
+    except:
+        try:
+            proceso9
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+
+    try:
+        proceso10();
+    except:
+        try:
+            proceso10
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+
+    try:
+        proceso11();
+    except:
+        try:
+            proceso11
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+    
+    try:
+        proceso12();
+    except:
+        try:
+            proceso12
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+    
+    try:
+        proceso13();
+    except:
+        try:
+            proceso13
         except:
             error = sys.exc_info()[1]
             print(error)

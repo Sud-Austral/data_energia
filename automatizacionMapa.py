b2fd237a3605f08a2359e8f5e8ff34b889202efd
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-12-22 10:33:44 -0600

    Commit

--- automatizacionMapa.py
+++ automatizacionMapa.py
@@ -1,6 +1,36 @@
 import pandas as pd
 import codecs
 import json
+import requests
+
+def descarga():
+    api_auth = "1594882b82550b038f365b0c6a7976682bdd0192"
+    url = 'https://api.desarrolladores.energiaabierta.cl/bencina-en-linea/v1/combustibles/vehicular/estaciones.json/'
+    get_url = f"{url}?auth_key={api_auth}&limit=5000"
+
+    response = requests.get(get_url)
+    decoded_data=codecs.decode(response.content, 'utf-8-sig')
+    d = json.loads(decoded_data, strict=False)
+
+    result = d
+    df = pd.DataFrame(result["data"])
+    df.columns = result["headers"]
+
+
+def string_to_float(cadena):
+    
+    try:
+        return float(cadena)
+    except:
+        return None
+
+def clearFloat():
+    clasificacion = ['Gasolina 93 $/L', 'Gasolina 97 $/L', 'Petróleo Diesel $/L', 'Gasolina 95 $/L', 'GLP Vehicular $/m3', 'GNC $/m3']
+    
+    for i in clasificacion:
+
+        df[i] = df[i].apply(string_to_float)
+
 
 def getTramos(df, variable):
     
@@ -34,74 +64,101 @@ def getTramos(df, variable):
     
     return
 
-def clear(texto):
+def clasificacion():
     
-    finalText = texto.replace("'", "").replace("´", "").replace("´", "").replace("’", "").replace(",", "").replace(";", "").replace("\\", "").replace("/", "").replace("[", "").replace("]", "").replace("{", "").replace("}", "")
-    return finalText
-
-def upgradePoints(dfAux):
-    
-    df = dfAux
-    
-    salida1 = 'puntos = {"type": "FeatureCollection","features": ['
-    salida3 = ']}'
-
-    data = []
-
-    for i,j in df.iterrows():
-
-        lat = df["Latitud"][i]
-        lng = df["Longitud"][i]
-
-        RAZON_SOCIAL = clear(str(df["Razón Social"][i]))
-        CALLE = clear(str(df["Calle"][i]))
-        NUMERO = df["Número"][i]
-        COMUNA = clear(str(df["Comuna"][i]))
-        REGION = clear(str(df["Región"][i]))
-        HORARIO_ATENCION = clear(str(df["Horario de Atención"][i]))
-        DISTRIBUIDOR = clear(str(df["Distribuidor"][i]))
-        D_LOGO_SVG = df["Distribuidor Logo SVG"][i]
-        G93 = df["Gasolina 93 $/L"][i]
-        G97 = df["Gasolina 97 $/L"][i]
-        PETROLEO = df["Petróleo Diesel $/L"][i]
-        G95 = df["Gasolina 95 $/L"][i]
-        GLP = df["GLP Vehicular $/m3"][i]
-        GNC = df["GNC $/m3"][i]
-        class93 = df['ClasificacionGasolina 93 $/L'][i]
-        class95 = df['ClasificacionGasolina 95 $/L'][i]
-        class97 = df['ClasificacionGasolina 97 $/L'][i]
-        classGLP = df['ClasificacionGLP Vehicular $/m3'][i]
-        classGNC = df['ClasificacionGNC $/m3'][i]
-        classD = df['ClasificacionPetróleo Diesel $/L'][i]
-
-        salida2 = '{"type": "Feature","geometry": {"type": "Point","coordinates": [' + str(lng) + ',' + str(lat) + ']},"properties": {"razon": "' + str(RAZON_SOCIAL) + '", "calle": "' + str(CALLE) + '", "numero": "' + str(NUMERO) + '", "comuna": "' + str(COMUNA) + '", "region": "' + str(REGION) + '", "horario": "' + str(HORARIO_ATENCION) + '", "distribucion": "' + str(DISTRIBUIDOR) + '", "logo": "' + str(D_LOGO_SVG) + '", "G93": "' + str(G93) + '", "G97": "' + str(G97) + '", "petroleo": "' + str(PETROLEO) + '", "G95": "' + str(G95) + '", "GLP": "' + str(GLP) + '", "GNC": "' + str(GNC) + '", "class93": "' + str(class93) + '", "class95": "' + str(class95) + '", "class97": "' + str(class97) + '", "classGLP": "' + str(classGLP) + '", "classGNC": "' + str(classGNC) + '", "classD": "' + str(classD) + '"}}'
-        data.append(salida2)
-
-    salida = (salida1 + str(data).replace("'", "").replace("[{", "{").replace("}}]", "}}") + salida3)
-    # print(salida)
-
-    with codecs.open('mapa/chile/establecimientos.js', 'w', 'utf-8') as file:
-        file.write(salida)
-        
-    print("Puntos actualizados")
-
-
-def establecimientos():
-    df = pd.read_excel('combustibles_vehicular_estaciones.xlsx')
-    
-    # df = df.head(50)
-
-    df = df[df['ID'] != 'pb520101']
-    df = df[df['ID'] != 'pb520102']
-
     clasificacion = ['Gasolina 93 $/L', 'Gasolina 97 $/L', 'Petróleo Diesel $/L', 'Gasolina 95 $/L', 'GLP Vehicular $/m3', 'GNC $/m3']
 
     for i in clasificacion:
 
         getTramos(df, i)
+    
+    createFile(df)
 
-    upgradePoints(df)
 
+def createFile(df):
+
+    salida = []
+
+    df = df[df['ID'] != 'pb520101']
+    df = df[df['ID'] != 'pb520102']
+
+    for i in range(len(df)):
+
+        filasJSON = json.loads(df.iloc[i].to_json(orient='index', force_ascii=False))
+
+        filasJSON['razon'] = filasJSON['Razón Social']
+        filasJSON['calle'] = filasJSON['Calle']
+        filasJSON['numero'] = filasJSON['Número']
+        filasJSON['comuna'] = filasJSON['Comuna']
+        filasJSON['region'] = filasJSON['Región']
+        filasJSON['horario'] = filasJSON['Horario de Atención']
+        filasJSON['distribucion'] = filasJSON['Distribuidor']
+        filasJSON['logo'] = filasJSON['Distribuidor Logo SVG']
+        filasJSON['G93'] = filasJSON['Gasolina 93 $/L']
+        filasJSON['G97'] = filasJSON['Gasolina 97 $/L']
+        filasJSON['petroleo'] = filasJSON['Petróleo Diesel $/L']
+        filasJSON['G95'] = filasJSON['Gasolina 95 $/L']
+        filasJSON['GLP'] = filasJSON['GLP Vehicular $/m3']
+        filasJSON['GNC'] = filasJSON['GNC $/m3']
+        filasJSON['class93'] = filasJSON['ClasificacionGasolina 93 $/L']
+        filasJSON['class95'] = filasJSON['ClasificacionGasolina 95 $/L']
+        filasJSON['class97'] = filasJSON['ClasificacionGasolina 97 $/L']
+        filasJSON['classGLP'] = filasJSON['ClasificacionGLP Vehicular $/m3']
+        filasJSON['classGNC'] = filasJSON['ClasificacionGNC $/m3']
+        filasJSON['classD'] = filasJSON['ClasificacionPetróleo Diesel $/L']
+
+        del filasJSON['Razón Social']
+        del filasJSON['Calle']
+        del filasJSON['Número']
+        del filasJSON['Comuna']
+        del filasJSON['Región']
+        del filasJSON['Horario de Atención']
+        del filasJSON['Distribuidor']
+        del filasJSON['Distribuidor Logo SVG']
+        del filasJSON['Gasolina 93 $/L']
+        del filasJSON['Gasolina 97 $/L']
+        del filasJSON['Petróleo Diesel $/L']
+        del filasJSON['Gasolina 95 $/L']
+        del filasJSON['GLP Vehicular $/m3']
+        del filasJSON['GNC $/m3']
+        del filasJSON['ClasificacionGasolina 93 $/L']
+        del filasJSON['ClasificacionGasolina 95 $/L']
+        del filasJSON['ClasificacionGasolina 97 $/L']
+        del filasJSON['ClasificacionGLP Vehicular $/m3']
+        del filasJSON['ClasificacionGNC $/m3']
+        del filasJSON['ClasificacionPetróleo Diesel $/L']
+
+        del filasJSON['ID']
+        del filasJSON['ID Comuna']
+        del filasJSON['ID Región']
+        del filasJSON['Distribuidor Logo']
+        del filasJSON['Distribuidor Logo SVG Horizontal']
+        del filasJSON['Tienda']
+        del filasJSON['Farmacia']
+        del filasJSON['Mantención']
+        del filasJSON['Autoservicio']
+        del filasJSON['Pago Efectivo']
+        del filasJSON['Cheque']
+        del filasJSON['Tarjetas Bancarias']
+        del filasJSON['Tarjeta Grandes Tiendas']
+
+
+        salida2 = {"type": "Feature","geometry": {"type": "Point","coordinates": [filasJSON['Longitud'], filasJSON['Latitud']]},"properties": filasJSON}
+        # print(salida2)
+
+        salida.append(salida2.copy())
+
+    jsonFinal = {"type": "FeatureCollection", "features": salida}
+
+    with open('chile/combustibles.json', 'w', encoding='utf8') as myfile:
+        json.dump(jsonFinal, myfile, ensure_ascii=False)
+    
+    print("Estaciones de combustible creado correctamente.")
+
+def descargaFinal():
+    descarga()
+    clearFloat()
+    clasificacion()
 
 if __name__ == '__main__':
-    establecimientos()
+    descargaFinal()

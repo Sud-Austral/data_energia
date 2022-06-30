d3f55ff1d0a3a8aa559d312062431459b74d0f10
lmonsalve22 <lmonsalve22@gmail.com>
2022-06-30 17:31:06 -0400

    Update automatizacion2.py

--- automatizacion2.py
+++ automatizacion2.py
@@ -9,6 +9,9 @@ def proceso():
     url = 'https://3b9x.short.gy/xAW5bI'
     file = req.get(url, allow_redirects=True)
     open('data.csv', 'wb').write(file.content)
+    df = pd.read_csv("data.csv", sep=";")
+    df.rename(columns = {'ID':'id'}, inplace = True)
+    df.to_csv("avance.csv", index=False)
     return
     
 

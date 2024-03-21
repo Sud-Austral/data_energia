43d4cd60c7579d4ff3aff8cdcf60b9ca3005d9b5
lmonsalve22 <lmonsalve22@gmail.com>
2024-03-21 18:59:04 -0300

    probar action

--- automatizacion2.py
+++ automatizacion2.py
@@ -15,6 +15,7 @@ def changeToNumber(number):
 
 def proceso():
     url = 'https://3b9x.short.gy/xAW5bI'
+    
     file = req.get(url, allow_redirects=True)
     open('data.csv', 'wb').write(file.content)
     df = pd.read_csv("data.csv", sep=";")
